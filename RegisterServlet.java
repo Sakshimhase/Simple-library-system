bd38d63e36bfb5a8a2390c46fcdbcaa13ffc48c9
Sakshimhase <sakshimhase309@gmail.com>
2025-05-23 15:32:34 +0530

    Add files via upload

---
+++ RegisterServlet.java
@@ -0,0 +1,74 @@
+package com.sp.in;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+
+	private static final String query="Insert into book(book_name, book_edition,book_price) VALUES(?,?,?)";
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter pw = resp.getWriter();
+		
+		resp.setContentType("text/html");
+		//get the book details
+
+        String bookname = req.getParameter("bookname");   
+        String bookedition = req.getParameter("bookedition");  
+        float bookprice = Float.parseFloat (req.getParameter("bookprice"));  
+
+
+		//load the driver
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}catch (ClassNotFoundException cnf) {
+			cnf.printStackTrace();
+		}
+		
+		//generate the connection
+		try(Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project_db","root","Sakshi@2829");
+		PreparedStatement ps=con.prepareStatement(query);){
+		ps.setString(1,bookname);
+		ps.setString(2, bookedition);
+		ps.setFloat(3, bookprice);
+		int count = ps.executeUpdate();
+		if(count==1) {
+			pw.println("<h2>registered sucessfully</h2>");
+		}else
+		{
+			pw.println("<h2>not registered sucessfully</h2>");
+
+		}
+		
+		}
+		catch (SQLException se) {
+			se.printStackTrace();
+			pw.println("<h1>"+ se.getMessage()+"<h2>");
+		}catch(Exception e) {
+			e.printStackTrace();
+			pw.println("<h1>"+ e.getMessage()+"<h2>");
+
+		}
+		pw.println("<a href='index.html'>Home</a>");
+		pw.println("<br>");
+		pw.println("<a href=\"booklist\">List of books</a>");
+	}
+	
+	//(type doget and (ctrl + space ) then automatically created
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req,resp);
+	}
+	
+}
